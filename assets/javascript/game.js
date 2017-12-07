f3bd720d39a4b0fa7c71cffc6c0780dd623d267d
nickolasbuchanan <fdnickolas@gmail.com>
2017-12-06 23:08:16 -0500

    JavaSript started with details on how to start the game plus ideas for variables that I will need.

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+//Variaibles need
+
+//Need to start an array with all the word choices.
+
+//The word chosen needs to be broken down to letters.
+
+//Those letters need to be turned into blanks.
+
+//Need a spot for the letters and unsloved blanks
+
+//Need a spot for the worng guess
+
+//Need a spot for correct guesses
+
+//Need counters for the game: win, loss, guesses left
